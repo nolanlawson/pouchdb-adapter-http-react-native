b31a89e0df49a8c493adc4991f32a41adba4d5a4
Nolan Lawson <nolan.lawson@gmail.com>
2016-09-26 20:26:59 +0100

    experiment

--- bin/prepublish.js
+++ bin/prepublish.js
@@ -1,28 +1,10 @@
-var rollup = require('rollup').rollup
-var nodeResolve = require('rollup-plugin-node-resolve-auto')
 var denodeify = require('denodeify')
-var mkdirp = denodeify(require('mkdirp'))
+var ncp = denodeify(require('ncp'))
 var rimraf = denodeify(require('rimraf'))
-var detective = require('detective')
 Promise.resolve().then(function () {
   return rimraf('lib')
 }).then(function () {
-  return mkdirp('lib')
-}).then(function () {
-  return rollup({
-    entry: './src/index.js',
-    plugins: [
-      nodeResolve({
-        jsnext: true,
-        browser: false
-      })
-    ]
-  })
-}).then(function (bundle) {
-  var code = bundle.generate({
-    format: 'cjs'
-  }).code
-  var requires = detective(code)
+  return ncp('node_modules/pouchdb-adapter-http/lib', 'lib')
 }).catch(function (err) {
   console.error(err)
   console.error(err.stack)
