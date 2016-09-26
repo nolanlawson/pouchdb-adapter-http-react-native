c2d397fcb2d49d54b855c7bd84c9c1b6f3b5fb74
Nolan Lawson <nolan.lawson@gmail.com>
2016-09-27 00:33:43 +0100

    working

--- bin/prepublish.js
+++ bin/prepublish.js
@@ -1,11 +1,14 @@
 var rollup = require('rollup').rollup
-var nodeResolve = require('rollup-plugin-node-resolve')
+var nodeResolve = require('rollup-plugin-node-resolve-auto')
 var denodeify = require('denodeify')
 var mkdirp = denodeify(require('mkdirp'))
 var rimraf = denodeify(require('rimraf'))
 var detective = require('detective')
+var builtins = require('builtin-modules')
 var fs = require('fs')
 var writeFile = denodeify(fs.writeFile)
+var readFile = denodeify(fs.readFile)
+var path = require('path')
 
 Promise.resolve().then(function () {
   return rimraf('lib')
@@ -16,13 +19,7 @@ Promise.resolve().then(function () {
     entry: './src/index.js',
     plugins: [
       nodeResolve({
-        jsnext: true,
-        browser: false,
-        skip: [
-          // TODO: use rollup-plugin-node-resolve-auto
-          'debug', 'request', 'js-extend', 'spark-md5',
-          'vuvuzela', 'argsarray', 'es6-promise-pool', 'lie', 'inherits'
-        ]
+        browser: false
       })
     ]
   })
@@ -32,10 +29,20 @@ Promise.resolve().then(function () {
   }).code
   var requires = detective(code)
   return Promise.all([
-    fs.writeFile('lib/index.js', code, 'utf8'),
-    fs.readFile('package.json', 'utf8').then(function (pkgJson) {
+    writeFile('lib/index.js', code, 'utf8'),
+    readFile('package.json', 'utf8').then(function (pkgJson) {
       var pkg = JSON.parse(pkgJson)
-      // todo
+      pkg.dependencies = {}
+      return Promise.all(requires.map(function (req) {
+        if (builtins.indexOf(req) !== -1) {
+          return
+        }
+        return readFile(path.join('node_modules', req, 'package.json'), 'utf8').then(function (otherPkg) {
+          pkg.dependencies[req] = JSON.parse(otherPkg).version
+        })
+      })).then(function () {
+        return writeFile('package.json', JSON.stringify(pkg, null, '  ') + '\n', 'utf8')
+      })
     })
   ])
 }).catch(function (err) {
