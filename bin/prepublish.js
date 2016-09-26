7637b8125188db91969819ff74558102dce6ebb6
Nolan Lawson <nolan.lawson@gmail.com>
2016-09-26 20:27:24 +0100

    back to old prepublish

--- bin/prepublish.js
+++ bin/prepublish.js
@@ -1,10 +1,28 @@
+var rollup = require('rollup').rollup
+var nodeResolve = require('rollup-plugin-node-resolve-auto')
 var denodeify = require('denodeify')
-var ncp = denodeify(require('ncp'))
+var mkdirp = denodeify(require('mkdirp'))
 var rimraf = denodeify(require('rimraf'))
+var detective = require('detective')
 Promise.resolve().then(function () {
   return rimraf('lib')
 }).then(function () {
-  return ncp('node_modules/pouchdb-adapter-http/lib', 'lib')
+  return mkdirp('lib')
+}).then(function () {
+  return rollup({
+    entry: './src/index.js',
+    plugins: [
+      nodeResolve({
+        jsnext: true,
+        browser: false
+      })
+    ]
+  })
+}).then(function (bundle) {
+  var code = bundle.generate({
+    format: 'cjs'
+  }).code
+  var requires = detective(code)
 }).catch(function (err) {
   console.error(err)
   console.error(err.stack)
